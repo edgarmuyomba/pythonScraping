76ccbb683659b0999ebdf7e297348ff0c9b137cb
edgarmatthew <edgarmatthew247@gmail.com>
2023-02-10 10:23:52 +0300

    adding mysql to wikiProfileScraper

--- wikiProfileScraper.py
+++ wikiProfileScraper.py
@@ -6,9 +6,15 @@ import re
 from requests import Session
 import time 
 from random import choice
+import pymysql
 
 socks.set_default_proxy(socks.SOCKS5, 'localhost', 9150)
 socket.socket = socks.socksocket
+
+#Setting up a connection to mysql database to store the profiles
+conn = pymysql.connect(host='127.0.0.1', user='root', passwd='muyomba', db='mysql', charset='utf8')
+cur = conn.cursor()
+cur.execute ('USE wikipediaProfiles')
 
 session = Session()
 headers = {
@@ -19,6 +25,9 @@ headers = {
 }
 
 class Content:
+    """
+        A class used to organise the details about the discovered profiles
+    """
     def __init__(self, url, name, dob, birthPlace):
         self.url = url.strip()
         self.name = name.strip()
@@ -35,6 +44,9 @@ class Content:
         )
 
 def getPage(url):
+    """
+        A function used to obtain the article section of a wikipedia page
+    """
     global session, headers 
     html = session.get(url, headers=headers)
     bs = BeautifulSoup(html.text, 'html.parser')
@@ -42,6 +54,9 @@ def getPage(url):
     return article 
 
 def getDetails(url):
+    """
+        A function used to obtain the details from the discovered profiles on wikipedia
+    """
     bs = getPage(url)
     time.sleep(2)
     try:
@@ -49,19 +64,35 @@ def getDetails(url):
         dob = bs.find('span', {'class': 'bday'}).get_text()
         birthPlace = bs.find('td', {'class': 'infobox-data birthplace'}).get_text()
     except AttributeError:
+        #if a page is not a profile page (for a person), the function simply returns None
         return None 
     else:
         content = Content(url, name, dob, birthPlace)
         content.printProfile()
+        #After printing the details, links to other profiles are discovered on the page
         getLinks(bs)
 
 def getLinks(bs):
+    """
+        A function to obtain links to other profiles/pages from a wikipedia page
+    """
     links = bs.find_all('a', href=re.compile('^(/wiki/).*'))
     for i in range(len(links)):
+        #A link to follow is selected at random and its details further obtained
         link = choice(links)
         url = link.attrs['href']
         url = 'https://en.wikipedia.org' + url 
         print(url,'\n')
         getDetails(url)
 
-print(getDetails('https://en.wikipedia.org/wiki/Christiano_Ronaldo'))
+def store(url, name, dob, birthPlace):
+    cur.execute('INSERT INTO Profiles (url, fullname, DOB, birthPlace) VALUES (%s, %s, %s, %s)', (url, name, dob, birthPlace))
+    cur.commit()
+
+if __name__ == '__main__':
+    try:
+        getDetails('https://en.wikipedia.org/wiki/Christiano_Ronaldo')
+    finally:
+        cur.close()
+        conn.close()
+
